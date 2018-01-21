3cda5dfd360ea2ff23fd7e167130aa2f1d6d0b40
Michael Gillam <jamargillam@gmail.com>
2018-01-21 12:14:20 -0600

    Started on hero template (full screen)

--- templates/hero/full-screen.php
+++ templates/hero/full-screen.php
@@ -1,1 +1,7 @@
-HERO :)
+<div class="uk-cover-container" uk-height-viewport>
+  <img src="<?php echo get_template_directory_uri().'/images/hero.jpg' ?>" alt="Hero" uk-cover>
+    <div class="uk-position-center uk-text-center">
+      <h1>SemanticPixels</h1>
+      <p>Results Driven. Data Driven.</p>
+    </div>
+</div>
